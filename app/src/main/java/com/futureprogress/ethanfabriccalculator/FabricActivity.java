dde8539e89269c6f69f77f1a6e42451ade87efa3
Suneil Basu <suneil@futureprogress.com>
2017-01-16 21:56:19 +0000

    Initial commit

---
+++ app/src/main/java/com/futureprogress/ethanfabriccalculator/FabricActivity.java
@@ -0,0 +1,13 @@
+package com.futureprogress.ethanfabriccalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FabricActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fabric);
+    }
+}
